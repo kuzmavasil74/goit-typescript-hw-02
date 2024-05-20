a2dfa09b6a227b1600b07458fdb929aef24e68c8
kuzmavasil74 <98240149+kuzmavasil74@users.noreply.github.com>
2024-05-20 22:27:09 +0200

    w

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import { createRoot } from 'react-dom'
+import ReactDOM from 'react-dom'
+
 import App from './App'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
